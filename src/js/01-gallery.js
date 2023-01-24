a122b0b7b501f24653324b3f0694378c5c33018d
Oleksandr Synook <ssineok21@gmail.com>
2023-01-24 07:18:58 +0200

    Update video

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+const createGalleryItem = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+  })
+  .join('');
+
+galleryList.insertAdjacentHTML('afterbegin', createGalleryItem);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDalay: 250,
+  captionsData: 'alt',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on(
+  'timeupdate',
+  _.throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.second);
+  }, 1000)
+);
